12d807a71a97555a4027b20877bdf3371816f842
somirayan <rayan.gacillos@propelrr.com>
2023-12-14 16:39:18 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,10 @@
 DOCKERFILE_PATH="/app/test-app/Dockerfile"
 
 docker stop lms-container || true
-sleep 10  # Wait for 10 seconds
-docker rm -f lms-container || true
-
+while [ "$(docker inspect -f '{{.State.Running}}' lms-container 2>/dev/null)" == "true" ]; do
+    sleep 1
+done
+docker rm lms-container || true
 
 # Build the Docker image
 docker build -t lms -f "$DOCKERFILE_PATH" .
